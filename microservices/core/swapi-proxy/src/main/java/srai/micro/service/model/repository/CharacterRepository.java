1115909798305ebd27ae3d65563cc6e432e1496f
Stuart Ingram <ingrams2@upmc.com>
2016-07-21 09:08:31 -0400

    Enhanced span tags for redis interaction from swapi-service

--- microservices/core/swapi-proxy/src/main/java/srai/micro/service/model/repository/CharacterRepository.java
+++ microservices/core/swapi-proxy/src/main/java/srai/micro/service/model/repository/CharacterRepository.java
@@ -3,6 +3,7 @@ package srai.micro.service.model.repository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -15,19 +16,39 @@ public class CharacterRepository implements Repository<CachableSwapiCharacter> {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(CharacterRepository.class);
 
+  @Value("${spring.redis.host:localhost}")
+  private String redisHostName;
+
+  @Value("${spring.redis.port:6379}")
+  private String redisPort;
+
+  private String redisURI;
+
   @Autowired
   RedisTemplate<String, CachableSwapiCharacter> redisTemplate;
 
   @Autowired
   Tracer tracer;
 
+  private String getRedisURI() {
+    if (this.redisURI == null) {
+      redisURI = redisHostName + ":" + redisPort;
+    }
+    return redisURI;
+  }
+
   @Override
   public void put(CachableSwapiCharacter character) {
     Span newSpan = this.tracer.createSpan("redis");
     try {
-      newSpan.tag("Operation", "write");
+      newSpan.tag("redis.op", "put");
+      newSpan.tag("lc", "redis");
+      newSpan.tag("redis.hostname", getRedisURI());
+      newSpan.logEvent(Span.CLIENT_SEND);
       redisTemplate.opsForHash().put(character.getObjectKey(), character.getKey(), character);
     } finally {
+      newSpan.tag("sa", getRedisURI());
+      newSpan.logEvent(Span.CLIENT_RECV);
       this.tracer.close(newSpan);
     }
   }
@@ -36,9 +57,14 @@ public class CharacterRepository implements Repository<CachableSwapiCharacter> {
   public void delete(CachableSwapiCharacter key) {
     Span newSpan = this.tracer.createSpan("redis");
     try {
-      newSpan.tag("Operation", "delete");
+      newSpan.tag("redis.op", "delete");
+      newSpan.tag("lc", "redis");
+      newSpan.tag("redis.hostname", getRedisURI());
+      newSpan.logEvent(Span.CLIENT_SEND);
       redisTemplate.opsForHash().delete(key.getObjectKey(), key.getKey());
     } finally {
+      newSpan.tag("sa", getRedisURI());
+      newSpan.logEvent(Span.CLIENT_RECV);
       this.tracer.close(newSpan);
     }
   }
@@ -47,9 +73,14 @@ public class CharacterRepository implements Repository<CachableSwapiCharacter> {
   public CachableSwapiCharacter get(CachableSwapiCharacter key) {
     Span newSpan = this.tracer.createSpan("redis");
     try {
-      newSpan.tag("Operation", "read");
+      newSpan.tag("redis.op", "get");
+      newSpan.tag("lc", "redis");
+      newSpan.tag("redis.hostname", getRedisURI());
+      newSpan.logEvent(Span.CLIENT_SEND);
       return (CachableSwapiCharacter) redisTemplate.opsForHash().get(key.getObjectKey(), key.getKey());
     } finally {
+      newSpan.tag("sa", getRedisURI());
+      newSpan.logEvent(Span.CLIENT_RECV);
       this.tracer.close(newSpan);
     }
   }
